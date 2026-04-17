5a25f6f21c5652a6d51a98883f364463970a43ac
Vaanya Gupta <vaanya700@gmail.com>
2026-04-17 17:38:52 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-from Preprocessing import clean_text, split_into_sentences
-from DomainDetection import detect_domain
-from Mapping2 import analyze_structural_sentiment
-import pandas as pd
-import math
-
-def run_main():
-    all_plot_data = []
-    df = pd.read_csv("mental_health_data.csv") 
